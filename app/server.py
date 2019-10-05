2b6759d81a506ced68461536576302679db3221b
YoichiN <yoichi_naka22@outlook.com>
2019-10-05 13:37:40 +0900

    mark '&&&&&&' before predict

--- app/server.py
+++ app/server.py
@@ -34,6 +34,7 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
+    print('&&&&&&&&&')
     prediction = learn1.predict(img)[0]
     return JSONResponse({'result': str(prediction)})
 
